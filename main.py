1332fa5ac3b6f66e6756ef0c22a43253e96a9aaa
Nguyễn Duy Hưng <hungnd63@os.viettel.com.vn>
2024-12-28 17:29:28 +0700

    chore: tách tìm kiếm tên và mood riêng

--- main.py
+++ main.py
@@ -37,44 +37,71 @@ def normalize_vietnamese_text(text):
 
 @st.cache_data
 def norm_data(df: DataFrame) -> DataFrame:
-    return df.applymap(normalize_vietnamese_text)
+    return df.map(normalize_vietnamese_text)
 
 # Search bar
-search_query = st.text_input("Search",
-                             placeholder="Search for song title, singer, etc.")
+# Chia làm 2 ô search theo chiều ngang
+col1, col2 = st.columns(2)
+with col1:
+    search_title_query = st.text_input("Search Title",
+                                       placeholder="Search for song title, singer, etc.")
+with col2:
+    search_theme_query = st.text_input("Search Theme",
+                                       placeholder="Search for song theme, tempo, mood, etc.")
 
 df = load_data()
 norm_df = norm_data(df.astype(str))
 # Filter data based on search query
 # query on certain columns except 'karaoke' and 'perform'
-query_columns = df.columns.difference(['karaoke', 'perform'])
-if search_query:
-    search_query = normalize_vietnamese_text(search_query)
-    filtered_df = df.loc[norm_df[query_columns].apply(lambda row: row.str.contains(search_query, case=False).any(), axis=1)]
+# query các thông tin dễ thấy nhất(thường để trên title một bài hát)
+query_title_columns = ['title', 'singer(s)', 'songwriter(s)']
+if search_title_query:
+    search_title_query = normalize_vietnamese_text(search_title_query)
+    filtered_df = df.loc[norm_df[query_title_columns].apply(lambda row: row.str.contains(search_title_query, case=False).any(), axis=1)]
 else:
     filtered_df = df
+# query on theme, tempo, mood
+query_theme_columns = ['theme', 'tempo', 'mood']
+if search_theme_query:
+    search_theme_query = normalize_vietnamese_text(search_theme_query)
+    filtered_df = filtered_df.loc[norm_df[query_theme_columns].apply(lambda row: row.str.contains(search_theme_query, case=False).any(), axis=1)]
 
 # Show data
-st.dataframe(
-        filtered_df.set_index('title', inplace=False),
+st.data_editor(
+        # st dataframe chỉ cho pin index
+        filtered_df.set_index('title'),
         column_config={
             # 'title': st.column_config.Column(
             #     pinned=True
             #     ),
             'karaoke': st.column_config.LinkColumn(
                 help='Link to karaoke video',
-                max_chars=50,
                 width="small",
                 ),
             'perform': st.column_config.LinkColumn(
                 help='Link to singer perform song',
-                max_chars=50,
                 width="small"
                 ),
-            'duet': st.column_config.CheckboxColumn(
+            'duet': st.column_config.SelectboxColumn(
                 help='is this Duet song?',
                 width="small"
-                )
+                ),
+            'theme': st.column_config.SelectboxColumn(
+                help='chủ đề bài hát',
+                width="small",
+                options=['tết', 'wedding', 'love']
+                ),
+            'tempo': st.column_config.SelectboxColumn(
+                help='tiết tấu bài hát',
+                width="small",
+                options=['slow', 'medium', 'fast']
+                ),
+            'mood': st.column_config.SelectboxColumn(
+                help='tâm trạng bài',
+                width="small",
+                options=["happy", "sad", "love"]
+                ),
             },
+        num_rows='dynamic'
         # hide_index=True,
         )
